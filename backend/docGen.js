231ce524f5ae91009e176859e9053aaaf7d6829b
Charlie Sparks <csparks@antioch.edu>
2019-07-09 10:37:42 -0400

    now accepts custom parameters into docGen

--- backend/docGen.js
+++ backend/docGen.js
@@ -1,32 +1,50 @@
-var JSZip = require('jszip');
-var Docxtemplater = require('docxtemplater');
+module.exports = (req) => {
+    var JSZip = require('jszip');
+    var Docxtemplater = require('docxtemplater');
 
-var fs = require('fs');
-var path = require('path');
+    var fs = require('fs');
+    var path = require('path');
 
-// loading the docx file as a binary
-var content = fs
-    .readFileSync(path.resolve(__dirname, 'input.docx'));
+    // loading the docx file as a binary
+    var content = fs
+        .readFileSync(path.resolve(__dirname, 'input.docx'));
 
-var zip = new JSZip(content);
-var doc = new Docxtemplater();
-doc.loadZip(zip);
+    var zip = new JSZip(content);
+    var doc = new Docxtemplater();
+    doc.loadZip(zip);
 
-// defining the template variables
-doc.setData({
-    first_name: "Charlie",
-    last_name: "Sparks",
-    description: "this is a test",
-    phone: '(111) 111-1111'
-});
+    // defining the template variables
+    if (req) {
+        const {
+            first_name,
+            last_name,
+            description,
+            phone
+        } = req;
 
-try {
-    doc.render();
-} 
-catch (err) {
-    console.log('error', err);
-};
+        doc.setData({
+            first_name,
+            last_name,
+            description,
+            phone
+        });
+    } else {
+        doc.setData({
+            first_name: "Charlie",
+            last_name: "Sparks",
+            description: "this is a test",
+            phone: '(111) 111-1111'
+        });
+    }
 
-var buf = doc.getZip().generate({ type: 'nodebuffer' });
+    try {
+        doc.render();
+    } 
+    catch (err) {
+        console.log('error', err);
+    };
 
-fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
+    var buf = doc.getZip().generate({ type: 'nodebuffer' });
+
+    fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
+}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const port = process.env.PORT || 3001;
+const docGen = require('./docGen');
 
 const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('<h1>get it</h1>');
+});
+
+server.get('/doc-gen', (req, res) => {
+    docGen();
+});
+
+server.post('/doc-gen', (req, res) => {
+    docGen(req.body);
 })
 
 server.listen(port, () => console.log(`The server is listening on port ${port}`));
